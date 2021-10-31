e519556f48eb139a4727079c5fe7f40bc01b247e
saufiroja <saufiroja08@gmail.com>
2021-10-31 16:22:14 +0800

    membuat fitur require auth

--- routes/router.controller.js
+++ routes/router.controller.js
@@ -1,13 +1,14 @@
 const { Router } = require("express");
 const controller = require("../controllers/profiler.controller");
+const requireAuth = require("../middlewares/auth.middleware");
 
 const router = Router();
 
 // get
 router.get("/register", controller.register);
 router.get("/login", controller.login);
-router.get("/biodata", controller.biodata);
-router.get("/history", controller.history);
+router.get("/biodata", requireAuth, controller.biodata);
+router.get("/history", requireAuth, controller.history);
 
 // post
 router.post("/register-user", controller.createRegister);
